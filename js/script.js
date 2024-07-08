50b2614a7cdd510cd92f044724907ce95317f708
elencodes <playnonstop205@gmail.com>
2024-07-09 02:50:16 +0400

    add changes

--- js/script.js
+++ js/script.js
@@ -31,33 +31,6 @@ const inputConditionClass = document.querySelector(`.input__box-radio--condition
 const inputOwnersClass = document.querySelector(`.input__box-radio--owners`);
 const inputPaymentClass = document.querySelector(`.input__box-radio-payment`);
 
-//4 Создаем массив с объектами, который содержит в себе цены для каждой новой модели авто
-const carModelPrice = [{
-	model: 'Duster',
-	price: '1868000'
-}, {
-	model: 'Kaptur',
-	price: '1724000'
-}, {
-	model: 'Astra',
-	price: '3200000'
-}, {
-	model: 'Corsa',
-	price: '3100000'
-}, {
-	model: 'RX8',
-	price: '3400000'
-}, {
-	model: '6',
-	price: '4600000'
-}, {
-	model: 'F-Type',
-	price: '7500000'
-}, {
-	model: 'F-Pace',
-	price: '12700000'
-}, ];
-
 
 //5 Корректировка выпадающего списка моделей авто при выборе определенной марки
 selectBrand.addEventListener(`change`, function (event) {
@@ -113,6 +86,8 @@ inputRadioConditionUsed.addEventListener(`change`, function (event) {
 //пункт выбора количества владельцев скрывается
 inputRadioConditionNew.addEventListener(`change`, function (event) {
 	if (inputRadioConditionNew.checked) {
+		inputRadioCarOwner.checked = false;
+		inputRadioCarOwners.checked = false;
 		blockOwnersVisible.style.display = 'none';
 	}
 });
@@ -312,8 +287,158 @@ inputEngineCapacity.addEventListener(`blur`, function (event) {
 	inputEngineCapacityClass.classList.remove('focus');
 });
 
+//4 Создаем массив с объектами, который содержит в себе цены для каждой новой модели авто
+const carModelPrice = [{
+	model: 'Duster',
+	price: '1868000'
+}, {
+	model: 'Kaptur',
+	price: '1724000'
+}, {
+	model: 'Astra',
+	price: '3200000'
+}, {
+	model: 'Corsa',
+	price: '3100000'
+}, {
+	model: 'RX8',
+	price: '3400000'
+}, {
+	model: '6',
+	price: '4600000'
+}, {
+	model: 'F-Type',
+	price: '7500000'
+}, {
+	model: 'F-Pace',
+	price: '12700000'
+}, ];
+
+//4 Создаем массив с объектами, который содержит в себе коэффициенты надбавки за объем двигателя
+const engineCapacityRate = [{
+	'1.6': 1
+}, {
+	'2': 1.4
+}, {
+	'3': 1.8
+}, ]
+
+// Функция расчета стоимости авто
+
+const calculateTotalPrice = (model, engineCapacity, conditionNew, conditionUsed) => {
+	model = selectModel.value;
+	engineCapacity = Number(inputEngineCapacity.value);
+	conditionNew = inputRadioConditionNew.value;
+	conditionUsed = inputRadioConditionUsed.value;
+
+	//модель
+	let modelPrice;
+	if (model === 'Duster') {
+		modelPrice = Number(carModelPrice[0].price); //Renault Duster
+	} else if (model === 'Kaptur') {
+		modelPrice = Number(carModelPrice[1].price); //Renault Kaptur
+	} else if (model === 'Astra') {
+		modelPrice = Number(carModelPrice[2].price); //Opel Astra
+	} else if (model === 'Corsa') {
+		modelPrice = Number(carModelPrice[3].price); //Opel Corsa
+	} else if (model === 'RX8') {
+		modelPrice = Number(carModelPrice[4].price); //Mazda RX8
+	} else if (model === '6') {
+		modelPrice = Number(carModelPrice[5].price); //Mazda 6
+	} else if (model === 'F-Type') {
+		modelPrice = Number(carModelPrice[6].price); //Jaguar F-Type
+	} else if (model === 'F-Pace') {
+		modelPrice = Number(carModelPrice[7].price); //Jaguar F-Pace
+	}
 
 
+	//объем двигателя
+	let engineCapacityPrice;
+	if (engineCapacity === 1.6) {
+		engineCapacityPrice = engineCapacityRate[0][1.6];
+	} else if (engineCapacity === 2) {
+		engineCapacityPrice = engineCapacityRate[1][2];
+	} else if (engineCapacity === 3) {
+		engineCapacityPrice = engineCapacityRate[2][3];
+	}
 
-mainForm.addEventListener(`change`, checkFormValidityBrand);
-mainForm.addEventListener(`change`, checkFormValidityEngineCapacity);
+	const newCarPrice = 1;
+	const usedCarPriceOwner = 0.8;
+	const usedCarPriceOwners = 0.6;
+
+	let conditionPrice;
+	if (conditionUsed === 'usedCar' && inputRadioCarOwner.checked) {
+		conditionPrice = usedCarPriceOwner;
+	} else if (conditionUsed === 'usedCar' && inputRadioCarOwners.checked) {
+		conditionPrice = usedCarPriceOwners;
+	} else if (conditionNew === 'newCar') {
+		conditionPrice = newCarPrice;
+	}
+
+	return modelPrice * engineCapacityPrice * conditionPrice;
+};
+
+
+//31 Проверка на валидность всех введенных данных пользователем
+//(событие change срабатывает при изменении значения элемента формы)
+mainForm.addEventListener(`change`, function (event) {
+	checkFormValidityBrand();
+	checkFormValidityEngineCapacity();
+	calculateTotalPrice();
+
+	//добавление стилей для кнопки (кнопка станет активной, если нет сообщений об ошибках и галочка стоит на согласии)
+	if (document.querySelectorAll('.error__message').length === 0 &&
+		selectModel.value !== "" &&
+		selectColor.value !== "" &&
+		inputRadioFluePetrol.checked &&
+		inputEngineCapacity.value !== "" &&
+		(inputRadioConditionNew.checked || inputRadioConditionUsed.checked && inputRadioCarOwner.checked || inputRadioConditionUsed.checked && inputRadioCarOwners.checked) &&
+		(inputRadioPaymentCard.checked || inputRadioPaymentCash.checked || inputRadioPaymentInvoice.checked)) {
+		submitButton.disabled = false;
+		submitButton.style.backgroundColor = "#6ae18f";
+		submitButton.style.color = "#FFFFFF";
+	} else {
+		//возврат стилей к исходному состоянию (кнопка станет неактивной, если есть сообщения об ошибках и галочка не стоит на согласии)
+		submitButton.disabled = true;
+		submitButton.style.backgroundColor = "#c7baba6f";
+		submitButton.style.color = "#33333369";
+	}
+});
+
+mainForm.addEventListener('submit', function (event) {
+	//предотвращаем перезагрузку страницы и автоматическую отправку данных
+	event.preventDefault();
+
+	checkFormValidityBrand();
+	checkFormValidityEngineCapacity();
+	calculateTotalPrice();
+
+	if (document.querySelectorAll('.error__message').length === 0 &&
+		selectModel.value !== "" &&
+		selectColor.value !== "" &&
+		inputRadioFluePetrol.checked &&
+		inputEngineCapacity.value !== "" &&
+		(inputRadioConditionNew.checked || inputRadioConditionUsed.checked && inputRadioCarOwner.checked || inputRadioConditionUsed.checked && inputRadioCarOwners.checked) &&
+		(inputRadioPaymentCard.checked || inputRadioPaymentCash.checked || inputRadioPaymentInvoice.checked)) {
+		submitButton.disabled = false;
+		submitButton.style.backgroundColor = "#FFFFFF";
+		submitButton.style.color = "#333333";
+		//выводим в консоль значения полей формы после отправки
+		console.log({
+			Brand: selectBrand.value,
+			Model: selectModel.value,
+			Color: selectColor.value,
+			Flue: inputRadioFluePetrol.value,
+			EngineCapacity: inputEngineCapacity.value,
+			Condition: inputRadioConditionNew.value || inputRadioCarOwner.value || inputRadioCarOwners.value,
+			Payment: inputRadioPaymentCard.value || inputRadioPaymentCash.value || inputRadioPaymentInvoice.value,
+		});
+		//очищаем форму
+		mainForm.reset();
+		//выводим сообщение для пользователя об успешной регистрации
+		alert(`Поздравляем!`)
+		//возврат стилей кнопки к исходному состоянию
+		submitButton.style.backgroundColor = "#ffffff6f";
+		submitButton.style.color = "#333333";
+	}
+});
